3f74bf9874c97eb0ec420e866a16826b3c908b57
enzian <github@michaelingold.ch>
2015-10-02 11:24:43 +0200

    added fine-grained interface definitions

--- common/interfaces.go
+++ common/interfaces.go
@@ -2,6 +2,25 @@ package common
 
 import "github.com/go-martini/martini"
 
+// A ServiceStub returns routing parameters for multiple services to live on the same endpoint
 type ServiceStub interface {
 	Route() martini.Router
+}
+
+// IServiceRegistry abstracts the management of service definitions and versions
+type IServiceRegistry interface {
+	CreateServiceDefinition(identifierCode string, uriPrefix string, displayName string) (ServiceDefinition, error)
+	CreateServiceVersion(identifierCode string, version string) (ServiceVersion, error)
+}
+
+// IApiRegistry abstracts the management of api revisions
+type IApiRegistry interface {
+	CreateApiVersion(version string, displayName string) (ServiceDefinition, error)
+	LinkServiceVersion(apiVersion string, serviceIdentifier string, serviceVersion string) error
+}
+
+// IHostRegistry abstracts the management of hosts for given services and versions
+type IHostRegistry interface {
+	AddHost(serviceBaseURI string, serviceIdentifier string, serviceVersion string, state string) (ServiceHost, error)
+	SetHostState(hostID string, state string)
 }
